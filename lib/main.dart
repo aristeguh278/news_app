70db5beb2bed244e0616d168da359fb475fce7dd
aristeguh278 <aristeguh278@gmail.com>
2023-05-23 09:22:44 +0700

    fixing news list screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:dicoding_news_app/article.dart';
 import 'package:dicoding_news_app/detail_page.dart';
 import 'package:flutter/material.dart';
+import 'screens/list_screens.dart';
 import 'styles.dart';
-import 'screens/learning_path.dart';
 
 void main() {
   runApp(const MyApp());
@@ -39,62 +39,15 @@ class MyApp extends StatelessWidget {
         ),
       ),
       // home: const LearningPathPage(),
-      initialRoute: NewsListPage.routeName,
+      initialRoute: NewsListScreen.routeName,
       routes: {
-        NewsListPage.routeName: (context) => const NewsListPage(),
+        NewsListScreen.routeName: (context) => const NewsListScreen(),
         ArticleDetailPage.routeName: (context) => ArticleDetailPage(
               article: ModalRoute.of(context)?.settings.arguments as Article,
             ),
         ArticleWebView.routeName: (context) => ArticleWebView(
               url: ModalRoute.of(context)?.settings.arguments as String,
             ),
-      },
-    );
-  }
-}
-
-class NewsListPage extends StatelessWidget {
-  static const routeName = '/article_list';
-
-  const NewsListPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('News App'),
-      ),
-      body: FutureBuilder<String>(
-        future:
-            DefaultAssetBundle.of(context).loadString('assets/articles.json'),
-        builder: (context, snapshot) {
-          final List<Article> articles = parseArticles(snapshot.data);
-          return ListView.builder(
-            itemCount: articles.length,
-            itemBuilder: (context, index) {
-              return _buildArticleItem(context, articles[index]);
-            },
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildArticleItem(BuildContext context, Article article) {
-    return ListTile(
-      contentPadding: const EdgeInsets.symmetric(
-        horizontal: 16.0,
-        vertical: 8.0,
-      ),
-      leading: Image.network(
-        article.urlToImage,
-        width: 100,
-      ),
-      title: Text(article.title),
-      subtitle: Text(article.author),
-      onTap: () {
-        Navigator.pushNamed(context, ArticleDetailPage.routeName,
-            arguments: article);
       },
     );
   }
